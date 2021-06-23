184db330373b07bf8951d656a88cca411818cb9a
Nuno Góis <github@nunogois.com>
2021-06-23 12:04:28 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # discord-gamedeals
-Sends top GameDeals posts to Discord
 
- - https://www.reddit.com/r/gamedeals.rss
- - Either JS or Python / Go script
- - node-cron?
- - https://www.npmjs.com/package/discord-webhook-node
+Periodically sends free / highly scored posts from r/GameDeals to Discord.
+
+# 📌 To Do
+
+- [x] Create repo;
+- [x] Get r/GameDeals JSON;
+- [x] Add local scan.json file for config and keeping track of sentPosts;
+- [x] Add minScore filter;
+- [x] Add freeRegex filter;
+- [x] Add Webhook;
+- [x] Send posts embedded to Discord (https://www.npmjs.com/package/discord-webhook-node);
---
+++ scan.js
@@ -0,0 +1,77 @@
+require('dotenv').config()
+
+const cron = require('node-cron')
+const axios = require('axios')
+const fs = require('fs')
+const { Webhook, MessageBuilder } = require('discord-webhook-node')
+const hook = new Webhook(process.env.WEBHOOK)
+
+fs.readFile('scan.json', (err, data) => {
+  if (err) console.error(err)
+  else {
+    const jsonData = JSON.parse(data)
+
+    cron.schedule(jsonData.cronSchedule, async () => {
+      console.log('scanning...')
+      try {
+        const { data } = await axios.get(
+          'https://www.reddit.com/r/gamedeals.json'
+        )
+        const posts = data.data.children.map(child => {
+          const { data: post } = child
+          return {
+            id: post.id,
+            title: post.title,
+            thumbnail: post.thumbnail,
+            url: post.url,
+            score: post.score,
+            permalink: post.permalink,
+            created_utc: post.created_utc
+          }
+        })
+
+        const filteredPosts = posts.filter(
+          post =>
+            !jsonData.sentPosts.includes(post.id) &&
+            (post.score >= jsonData.minScore ||
+              (jsonData.freeRegex &&
+                new RegExp(jsonData.freeRegex, 'gi').test(post.title)))
+        )
+
+        jsonData.sentPosts.push(...filteredPosts.map(post => post.id))
+
+        filteredPosts.forEach(post => {
+          console.log(post)
+          const embed = new MessageBuilder()
+            .setTitle(post.title)
+            .setURL('https://www.reddit.com' + post.permalink)
+            .setColor('#3266a9')
+            .setDescription(
+              jsonData.freeRegex &&
+                new RegExp(jsonData.freeRegex, 'gi').test(post.title)
+                ? 'Free game! 😁🎉'
+                : 'Might be a good deal 🤔'
+            )
+            .setImage(
+              post.thumbnail.startsWith('http')
+                ? post.thumbnail
+                : 'https://github.com/nunogois/discord-gamedeals/blob/master/public/images/thumbnail.png?raw=true'
+            )
+            .setFooter(
+              'discord-gamedeals',
+              'https://github.com/nunogois/discord-gamedeals/blob/master/public/images/thumbnail.png?raw=true'
+            )
+            .setTimestamp()
+
+          hook.send(embed).catch(err => console.error(err))
+        })
+
+        fs.writeFile('scan.json', JSON.stringify(jsonData, null, 2), err => {
+          if (err) console.error(err)
+        })
+      } catch (err) {
+        console.error(err)
+      }
+    })
+  }
+})
